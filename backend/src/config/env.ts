17416d10a7ee8ef77d222288bb9927964941616f
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-05 18:47:51 +0300

    refactor(backend): add file config/env.ts for environment variables

---
+++ backend/src/config/env.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+
+export const env = {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  DB_USER: process.env.DB_USER!,
+  DB_PASSWORD: process.env.DB_PASSWORD!,
+  DB_HOST: process.env.DB_HOST!,
+  DB_PORT: Number(process.env.DB_PORT),
+  DB_NAME: process.env.DB_NAME!,
+};
