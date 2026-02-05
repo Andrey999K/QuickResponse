1ce3644ea261674f909e18e30feff557716776fb
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-05 18:49:09 +0300

    feat(backend): add DB connection

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,12 +1,13 @@
 // db/connection.ts
 import { Pool } from 'pg';
+import { env } from "../config/env";
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  host: env.DB_HOST,
+  port: Number(env.DB_PORT),
+  database: env.DB_NAME,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,21 @@
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
 import { testMiddleware } from "./testMiddleware";
 import helmet from "helmet";
 import compression from "compression";
+import { testConnection } from "./db/connection";
+import { env } from "./config/env";
 
 const app = express();
 
-dotenv.config();
-
-const port = process.env.PORT || 3000;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+const port = env.PORT || 3000;
 
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
 
 async function main() {
+  await testConnection()
+
   app.get('/', testMiddleware, (_req: Request, res: Response) => {
     res.send('Hello World!');
   });
