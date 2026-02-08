34b303c81d4dcc33288958b1b3d396ef3b90aeb4
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-08 17:55:11 +0300

    feat(backend): add middleware for logs

--- backend/src/middleware/corsMiddleware.ts
+++ backend/src/middleware/corsMiddleware.ts
@@ -1,22 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
-import { env } from "./config/env";
+import { env } from "../config/env";
 
 const allowedOrigin = env.NODE_ENV === 'production'
   ? 'https://myapp.com'
   : 'http://localhost:3000';
 
 export const corsMiddleware = async (
-  req: Request,
+  _req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   res.set({
-    // 'Cross-Origin-Opener-Policy': 'same-origin',
-    // 'Cross-Origin-Resource-Policy': 'same-origin',
     'Access-Control-Allow-Origin': allowedOrigin,
     'Access-Control-Allow-Headers': '*',
-    // 'Access-Control-Allow-Methods': ["POST"],
   });
-  console.log('Request:', req.method, req.path);
   next();
 };
