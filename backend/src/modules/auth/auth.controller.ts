8576cb320c8258b2b91576a42790c112eac61ad3
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-09 21:17:32 +0300

    feat(backend): added user existence check during registration

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -13,11 +13,15 @@ router.post("/signup", async (req: Request, res: Response) => {
       return res.status(400).json({ message: JSON.parse(validation.error?.message)[0].message });
     }
     const { email, username, password } = req.body;
-    const newUser = await userService.createUser(email, username, password);
-    return res.status(201).json(newUser);
+    const userExists = await userService.userExists(email);
+    if (!userExists) {
+      const newUser = await userService.createUser(email, username, password);
+      return res.status(201).json(newUser);
+    }
+    return res.status(409).json({ message: "Почта или ник уже заняты другим пользователем." });
   } catch (error) {
     console.log("error", error);
-    return res.status(409).json({ message: "Почта или ник уже заняты другим пользователем." });
+    return res.status(500).json({ message: "Непредвиденная ошибка сервера!" });
   }
 });
 
--- backend/src/modules/users/user.service.ts
+++ backend/src/modules/users/user.service.ts
@@ -21,6 +21,34 @@ export class UserService {
     }
   }
 
+  // async getUser(email: string): Promise<User | null> {
+  //   try {
+  //     const query = {
+  //       text: "SELECT * FROM users WHERE email = $1",
+  //       values: [email]
+  //     }
+  //     const result = await pool.query(query);
+  //     return result.rows[0];
+  //   } catch (error) {
+  //     console.error(error);
+  //     throw new Error("Error get users");
+  //   }
+  // }
+
+  async userExists(email: string): Promise<boolean> {
+    try {
+      const query = {
+        text: "SELECT EXISTS(SELECT 1 FROM users WHERE email = $1) as exists",
+        values: [email]
+      }
+      const result = await pool.query(query);
+      return result.rows[0].exists;
+    } catch (error) {
+      console.error('Error checking user existence:', error);
+      throw new Error("Error checking user");
+    }
+  }
+
   getAllUsers(): Promise<{ rows: User[] }> {
     try {
       return pool.query('SELECT * FROM users');
