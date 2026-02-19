3efd374bf1b71ec87df5336d90f4dffcd7c9174d
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-19 23:41:57 +0300

    feat(backend): validate email and password presence in middleware

--- backend/src/middleware/validation.middleware.ts
+++ backend/src/middleware/validation.middleware.ts
@@ -5,15 +5,18 @@ import { logger } from "@/utils/log";
 export const validate = (schema: z.ZodSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      schema.parse(req.body);
-      return next();
+      if ("email" in req.body && "password" in req.body) {
+        schema.parse(req.body);
+        return next();
+      }
+      return res.status(400).json({ message: "Email and password are required" });
     } catch (error) {
       if (error instanceof ZodError) {
         try {
           const errors: object[] = JSON.parse(error.message).map(
             (error: { message: string }) => ({ message: error.message })
           );
-          return res.status(500).json(errors);
+          return res.status(422).json(errors);
         } catch (error) {
           logger.error("Error parsing Zod validation errors: " + error);
           return res.status(500).json({ message: "Internal server error" });
