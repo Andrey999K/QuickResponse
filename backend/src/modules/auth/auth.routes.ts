dfc11479ed51c48ec3efe8f5fc71d4173f1f053c
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-19 23:43:00 +0300

    fix(backend): replace createUserDto on loginUserDto for login route

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { loginUser, signupUser } from "@/modules/auth/auth.controller";
-import { createUserDto } from "@/modules/users/user.dto";
+import { createUserDto, loginUserDto } from "@/modules/users/user.dto";
 import { validate } from "@/middleware/validation.middleware";
 
 const router = Router();
 
-router.post("/login", validate(createUserDto), loginUser);
+router.post("/login", validate(loginUserDto), loginUser);
 router.post("/signup", validate(createUserDto), signupUser);
 
 export const authRoutes = router;
