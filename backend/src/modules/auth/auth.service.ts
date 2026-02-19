789c28bff6b32309c9d13b06f847e4b65a1ebad9
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-19 23:46:42 +0300

    refactor(backend): removed try catch block in validateUser

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -4,26 +4,21 @@ import bcrypt from "bcrypt";
 
 export class AuthService {
   async validateUser(email: string, password: string): Promise<User | null> {
-    try {
-      const query = {
-        text: "SELECT * FROM users WHERE email = $1",
-        values: [email],
-      };
-      const result = await pool.query(query);
-      const user = result.rows[0];
+    const query = {
+      text: "SELECT * FROM users WHERE email = $1",
+      values: [email],
+    };
+    const result = await pool.query(query);
+    const user = result.rows[0];
 
-      if (!user) {
-        return null;
-      }
-
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return null;
-      }
-      return user;
-    } catch (error) {
-      console.error("Validate user error:", error);
-      throw new Error("Error validating user");
+    if (!user) {
+      return null;
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return null;
+    }
+    return user;
   }
 }
