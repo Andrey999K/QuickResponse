9f71973bb1235e6ff4dabb2b79d9976185eff35a
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-26 20:02:16 +0300

    feat(frontend): add component AuthButton

---
+++ frontend/src/components/UI/AuthButton.tsx
@@ -0,0 +1,35 @@
+import { Button } from "antd";
+import Link from "next/link";
+
+type AuthButtonProps = {
+  text?: string;
+  primary?: boolean;
+  arrow?: boolean;
+};
+
+export const AuthButton = async ({
+  text,
+  primary = true,
+  arrow,
+}: AuthButtonProps) => {
+  // const user = await getCurrentUser();
+  const user = null;
+  const textButton =
+    text || (user ? "Перейти к откликам" : "Войти через hh.ru");
+
+  return (
+    <>
+      <Link href={"/dashboard/search"}>
+      {/*<Link href={user ? "/dashboard/search" : generateAuthUrl()}>*/}
+        <Button
+          type={primary ? "primary" : "default"}
+          className="!font-semibold"
+          size="large"
+        >
+          {textButton}
+          {arrow && <span className="text-2xl"> &rarr;</span>}
+        </Button>
+      </Link>
+    </>
+  );
+};
