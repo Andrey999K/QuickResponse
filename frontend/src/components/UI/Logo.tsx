e8204cecc508f12ec19ecf7abaa3c307e2c49f5a
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-26 20:01:51 +0300

    feat(frontend): add component Logo

---
+++ frontend/src/components/UI/Logo.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export const Logo = () => {
+  return (
+    <Link
+      href="/"
+      className="flex items-center gap-2 !text-black text-base font-semibold"
+    >
+      <div className="w-[30px] h-[30px] bg-primary-500 rounded-lg">
+        {/*<Image src="/" alt="Logo" width={30} height={30} />*/}
+      </div>
+      Auto Offer
+    </Link>
+  );
+};
